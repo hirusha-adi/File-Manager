723aeb888ea00d57c0f6ffb01833d442a55649c9
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-03 18:19:51 +0530

    open file from default app in windows

--- files.py
+++ files.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template, redirect, send_file, request, url_for
-import sys
+
 import os
+import sys
+from tkinter import E
 import urllib.parse
+
+from flask import Flask, redirect, render_template, request, send_file, url_for
 
 WIN = True if os.name == 'nt' else False
 UNIX = False if os.name == 'nt' else True
+
+if WIN:
+    import shlex
+    import winreg
+
+    def get_default_windows_app(suffix):
+        try:
+            class_root = winreg.QueryValue(winreg.HKEY_CLASSES_ROOT, suffix)
+            try:
+                with winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, r'{}\shell\open\command'.format(class_root)) as key:
+                    command = winreg.QueryValueEx(key, '')[0]
+                    name = shlex.split(command)[0]
+                    if (name is None) or (name == '') or (bool(name) == False):
+                        return False
+                    else:
+                        return name
+            except:
+                return False
+        except:
+            return False
 
 
 app = Flask(__name__)
@@ -46,7 +69,15 @@ def file():
     if path is None:
         return "Bug lol"
     corrected = '/'.join(str(path).split("s1a5h"))
-    return send_file(corrected)
+    if WIN:
+        code = get_default_windows_app(corrected[-1])
+        if code is False:
+            return send_file(corrected)
+        else:
+            os.system(f'"{code}" "{path}"')
+            return "Program should be running"
+    else:
+        return send_file(corrected)
 
 
 @app.route("/changedir")
@@ -54,10 +85,15 @@ def changedir():
     path = request.args.get("destination")
     if path is None:
         return "Bug lol"
-    if path == "backback":
-        os.chdir("..")
     if path == "backbackback":
-        os.chdir("../..")
+        if WIN:
+            os.chdir("..\..")
+        else:
+            os.chdir("../..")
+    elif path == "backback":
+        os.chdir("..")
+    else:
+        os.chdir('/'.join(str(path).split("s1a5h")))
     return redirect(url_for('index'))
 
 
