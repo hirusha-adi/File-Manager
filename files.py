c509744574163d9ddfaad73ba66c45d69ed57026
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-02-03 20:09:03 +0530

    pls help. lol

--- files.py
+++ files.py
@@ -1,7 +1,6 @@
 
 import os
 import sys
-from tkinter import E
 import urllib.parse
 
 from flask import Flask, redirect, render_template, request, send_file, url_for
@@ -37,9 +36,12 @@ def index():
                 cwd_files[file] = urllib.parse.quote_plus(r's1a5h'.join(
                     os.path.join(os.getcwd(), file).split("/")))
 
+    current_folder_name = os.path.split(os.getcwd())[-1]
+
     return render_template("index.html",
                            cwd_files=cwd_files,
-                           cwd_folder=cwd_folder
+                           cwd_folder=cwd_folder,
+                           current_folder_name=current_folder_name
                            )
 
 
@@ -66,6 +68,21 @@ def changedir():
         os.chdir("..")
     else:
         os.chdir('/'.join(str(path).split("s1a5h")))
+    return redirect(url_for('index'))
+
+
+@app.route("/mkdir")
+def mkdir():
+    fname = request.args.get("name")
+    os.makedirs(fname)
+    return redirect(url_for('index'))
+
+
+@app.route("/mkfile")
+def mkdir():
+    fname = request.args.get("name")
+    with open(fname, "w") as ft:
+        ft.write()
     return redirect(url_for('index'))
 
 
